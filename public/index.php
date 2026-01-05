6585c1cc6f0491e5a258cd10fcc39e443bb3d29e
Pantaleon-Arkady <arkady.kidoslasher@gmail.com>
2026-01-05 14:16:05 +0800

    Favicon

--- public/index.php
+++ public/index.php
@@ -5,7 +5,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Init</title>
+    <link rel="icon" type="image/png" href="files/myicon.png">
+    <title>Portfolio</title>
     <link href="statics/style.css" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -15,7 +16,6 @@
 <body>
     <!-- HEADER -->
     <div class="two_main_divs header_div">
-        Header
     </div>
 
     <!-- MAIN -->
